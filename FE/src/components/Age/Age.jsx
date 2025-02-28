fe5b6da12344720e0ca239583a7948abd604f05e
Jont251 <nevgeny90@gmail.com>
2025-02-28 11:58:26 +0200

    added the Age component

---
+++ FE/src/components/Age/Age.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Age.css';
+
+const Age = ({ ageRating }) => {
+  return (
+    <div className="age-balloon">
+      <span className="age-text">{ageRating || '12+'}</span>
+    </div>
+  );
+};
+
+export default Age;
